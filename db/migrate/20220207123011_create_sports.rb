424d85d0c67fd060fa6b40bb83529aa757214f86
MansiJPawar <mansipawar517@gmail.com>
2022-02-07 19:06:34 +0530

    user and admin login with avatar , sport module with user and admin restrictions

---
+++ db/migrate/20220207123011_create_sports.rb
@@ -0,0 +1,11 @@
+class CreateSports < ActiveRecord::Migration[5.2]
+  def change
+    create_table :sports do |t|
+      t.string :sport_name
+      t.text :sport_equipement
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
