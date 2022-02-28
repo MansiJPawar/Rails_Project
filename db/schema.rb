4bf1e08fdd477ab8c4e5b8534784bd8b2c43b14c
MansiJPawar <mansipawar517@gmail.com>
2022-02-28 16:31:47 +0530

    product model setup with association done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_28_103919) do
+ActiveRecord::Schema.define(version: 2022_02_28_105112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,4 +48,14 @@ ActiveRecord::Schema.define(version: 2022_02_28_103919) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.integer "cost"
+    t.bigint "organization_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["organization_id"], name: "index_products_on_organization_id"
+  end
+
+  add_foreign_key "products", "organizations"
 end
