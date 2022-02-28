321333812049f97021c3c110909c99e419968d0d
MansiJPawar <mansipawar517@gmail.com>
2022-02-28 17:51:05 +0530

    product form design changed

--- app/admin/organizations.rb
+++ app/admin/organizations.rb
@@ -1,5 +1,15 @@
 ActiveAdmin.register Organization do
+  permit_params :name,  product_ids: []
 
+  show title: 'My Organization' do
+    h3 'You have '+ pluralize(organization.products.count, ' product') + ' for this organization'
+    organization.products.each do |product|
+      div do
+        h1 product.title
+        h4 link_to "[#{product.id}]: You created this product on #{product.created_at.strftime("%a, %b, %y at %I%M %p")}", admin_product_path(product)
+      end 
+    end
+  end
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,5 +1,14 @@
 ActiveAdmin.register Product do
+  permit_params :name, :cost, :organization_id
 
+  form title: 'Product App' do |f|
+    f.inputs 'Product' do
+      f.input :organization
+      f.input :name
+      f.input :cost
+    end
+    f.actions
+  end
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
